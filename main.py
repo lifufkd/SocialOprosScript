4e13af70851cff00710ffede1c71328de16dbfd3
lilfuz <lilfuz@gmail.com>
2024-03-19 22:41:46 +0300

    I won.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from config_parser import ConfigParser
 from data import Variants
+from selenium.webdriver.common.action_chains import ActionChains
 from deep_translator import GoogleTranslator
 ####################################################################
 config_name = 'config.json'
@@ -71,32 +72,83 @@ class Solver:
         except:
             sys.exit('Произошла ошибка в загрузке сайта, необходим перезапуск')
 
-    def insert_text(self, index):
-        answer = chat_gpt.gpt_query(worker.get_card_text(index + 1))
+    def insert_text(self, index, type):
+        answer = chat_gpt.gpt_query(worker.get_card_text(index + 1, type))
         answer_russian = GoogleTranslator(source='en', target='ru').translate(text=answer)
-        worker.insert_text(index + 1, answer_russian)
+        worker.insert_text(index + 1, answer_russian, type)
+
+    def any_choose(self, fixed=None):
+        branch = worker.get_buttons_1(True)
+        if fixed is None:
+            l_branch = len(branch)
+        else:
+            l_branch = fixed
+        choose = random.randint(1, l_branch)
+        for i in range(choose):
+            rnd = random.choice(branch)
+            branch.remove(rnd)
+            worker.set_button_1(rnd)
+        worker.confirm()
+        return choose
 
     def second_step(self):
         try:
-            main_branch = worker.get_buttons_1(True)
-            choose = random.randint(1, len(main_branch))
-            for i in range(choose):
-                rnd = random.choice(main_branch)
-                main_branch.remove(rnd)
-                worker.set_button_1(rnd)
-            worker.confirm()
-            for i in range(6):
+            choose = self.any_choose()
+            for i in range(11):
                 match i:
                     case 0:
-                        for g in range(19 - choose):
-                            self.insert_text(g)
+                        if choose != 19:
+                            for g in range(19 - choose):
+                                self.insert_text(g, 0)
+                            worker.confirm()
                     case 1:
                         buttons = worker.get_buttons_1(True)
                         choise = random.choice(buttons)
                         worker.set_button_1(choise)
                         worker.confirm()
                     case 2:
-                        self.insert_text(1)
+                        self.insert_text(0, 1)
+                        worker.confirm()
+                    case 3:
+                        if choose == 1:
+                            type = 1
+                        else:
+                            type = 0
+                        for g in range(choose):
+                            choise = random.randint(1, 8)
+                            worker.set_button_2(links.cards_buttons_query(type, g+1, choise))
+                        worker.confirm()
+                    case 4:
+                        self.any_choose(3)
+                    case 5:
+                        self.any_choose(3)
+                    case 6:
+                        for g in range(choose):
+                            choise = random.randint(1, 11)
+                            worker.set_button_2(links.circles(0, choise))
+                            worker.confirm()
+                            time.sleep(1)
+                            if worker.checker(0):
+                                worker.confirm()
+                            else:
+                                self.any_choose()
+                            choise = random.randint(1, 5)
+                            worker.set_button_2(links.circles(0, choise))
+                            worker.confirm()
+                    case 7:
+                        worker.confirm()
+                    case 8:
+                        for g in range(11):
+                            choise = random.randint(1, 5)
+                            worker.set_button_2(links.circles(1, choise, g + 1))
+                        worker.confirm()
+                    case 9:
+                        buttons = worker.get_buttons_1()
+                        choise = random.choice(buttons)
+                        worker.set_button_1(choise)
+                        worker.confirm()
+                    case 10:
+                        worker.insert_text(1, str(random.randint(18, 99)), 1)
                         worker.confirm()
 
         except Exception as e:
@@ -113,7 +165,10 @@ class Driver:
             'Я не веду страницу, блог или сообщество в Интернете',
             'Это решают другие люди',
             'Ничего из перечисленного',
-            'Другое:'
+            'Другое:',
+            'Менее 1 тыс. подписчиков',
+            'Собственный сайт, приложение',
+            'VK Видео'
         ] # 1. 1(A), 2(S), 3(A), 4(S), 5(A), 6(S), 7()
           # 2. 1(A), 2(S), 3(A), 4(S), 5(A), 6(S), 7()
           # 3. 1(S), 2(A), 3(A), 4(S), 5(A), 6(S), 7(A), 9()
@@ -128,12 +183,12 @@ class Driver:
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")  # linux only
-        chrome_options.add_argument("--headless=new")
+        #chrome_options.add_argument("--headless=new")
         self.__driver = webdriver.Firefox(service=service, options=chrome_options)
 
     def start(self):
         self.__driver.get(args.get_config()['url'])
-        time.sleep(7)
+        time.sleep(5)
         self.__driver.find_element(By.XPATH, f"//div[contains(text(), 'Начать опрос')]").click()
         time.sleep(1)
 
@@ -144,39 +199,93 @@ class Driver:
 
     def get_buttons_1(self, use_delete=False):
         output = list()
-        data = self.__driver.find_element(By.XPATH, "/html[@class=' json csscalc no-touchevents cssvhunit cssvwunit cssanimations flexbox']/body/div[@id='panel2_mainContainer']/div/div[@class='oprosso-poll']/div[@class='oprosso-poll__wrapper']/div[@class='oprosso-poll__horizontallContainer']/span/div/div[1]/div/div/div[@class='oprosso-poll__question-wrapper']/div[@class='oprosso-poll__question-wrapper-inner oprosso-poll__question-wrapper-inner_substrate']/div/div[@class='oprosso-poll__question-body']/div[@class='oprosso-poll-type__closed-wrapper']").text.split('\n')
-        for i in data:
-            if i not in self.__denied:
-                if use_delete:
-                    data = self.string_cleaner(i)
-                    if data is not None:
-                        output.append(data)
-                    else:
-                        output.append(i)
-                else:
-                    output.append(i)
-        return output
+        while True:
+            try:
+                data = self.__driver.find_element(By.XPATH, "/html[@class=' json csscalc no-touchevents cssvhunit cssvwunit cssanimations flexbox']/body/div[@id='panel2_mainContainer']/div/div[@class='oprosso-poll']/div[@class='oprosso-poll__wrapper']/div[@class='oprosso-poll__horizontallContainer']/span/div/div[1]/div/div/div[@class='oprosso-poll__question-wrapper']/div[@class='oprosso-poll__question-wrapper-inner oprosso-poll__question-wrapper-inner_substrate']/div/div[@class='oprosso-poll__question-body']/div[@class='oprosso-poll-type__closed-wrapper']").text.split('\n')
+                for i in data:
+                    if i not in self.__denied:
+                        if use_delete:
+                            data = self.string_cleaner(i)
+                            if data is not None:
+                                output.append(data)
+                            else:
+                                output.append(i)
+                        else:
+                            output.append(i)
+                return output
+            except Exception as e:
+                print(e)
 
     def set_button_1(self, text):
-        time.sleep(0.5)
-        self.__driver.find_element(By.XPATH, f"//div[contains(text(), '{text}')]").click()
+        while True:
+            try:
+                time.sleep(1)
+                element = self.__driver.find_element(By.XPATH, f"//div[contains(text(), '{text}')]")
+                self.__driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                ActionChains(self.__driver).move_to_element(element).click().perform()
+                time.sleep(1)
+                break
+            except Exception as e:
+                print(e)
+
+    def set_button_2(self, text):
+        while True:
+            try:
+                time.sleep(1)
+                element = self.__driver.find_element(By.XPATH, text)
+                self.__driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                ActionChains(self.__driver).move_to_element(element).click().perform()
+                time.sleep(1)
+                break
+            except Exception as e:
+                print(e)
 
     def confirm(self):
-        time.sleep(0.5)
-        self.__driver.find_element(By.XPATH, f"//span[contains(text(), 'Далее')]").click()
-        time.sleep(0.5)
+        while True:
+            try:
+                time.sleep(1)
+                element = self.__driver.find_element(By.XPATH, f"/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div/a/span")
+                self.__driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                ActionChains(self.__driver).move_to_element(element).click().perform()
+                time.sleep(1)
+                break
+            except Exception as e:
+                print(e)
 
-    def insert_text(self, index, text):
-        time.sleep(0.5)
-        container = self.__driver.find_element(By.XPATH, f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{index}]/div/div")
-        container.find_element(By.TAG_NAME, 'input').send_keys(text)
-        time.sleep(0.5)
+    def checker(self, type):
+        try:
+            time.sleep(1)
+            element = self.__driver.find_element(By.XPATH, links.checker(type))
+            answer_russian = GoogleTranslator(source='en', target='ru').translate(
+                text=chat_gpt.gpt_query('Поясните, почему Вы выбрали такой ответ'))
+            self.__driver.execute_script("arguments[0].scrollIntoView(true);", element)
+            element.find_element(By.TAG_NAME, 'textarea').send_keys(answer_russian)
+            time.sleep(1)
+            return True
+        except Exception as e:
+            print(e)
 
-    def get_card_text(self, index):
-        time.sleep(0.5)
-        text = self.__driver.find_element(By.XPATH, f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{index}]/div/div").text
-        time.sleep(0.5)
-        return text
+    def insert_text(self, index, text, type):
+        while True:
+            try:
+                time.sleep(1)
+                container = self.__driver.find_element(By.XPATH, links.cards_input_query(type, index))
+                self.__driver.execute_script("arguments[0].scrollIntoView(true);", container)
+                container.find_element(By.TAG_NAME, 'input').send_keys(text)
+                time.sleep(1)
+                break
+            except Exception as e:
+                print(e)
+
+    def get_card_text(self, index, type):
+        while True:
+            try:
+                time.sleep(1)
+                text = self.__driver.find_element(By.XPATH, links.cards_input_query(type, index)).text
+                time.sleep(1)
+                return text
+            except Exception as e:
+                print(e)
 
 
 class ChatGpt:
